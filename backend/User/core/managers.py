47f282930604ff811858556fba3c238ed5205a7a
river portatil <riverflorez.04@gmail.com>
2025-05-23 10:43:22 -0500

    backend added

--- backend/User/core/managers.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class UserManager(models.Manager):
-    def create_user(self, name, email, password):
-        user = self.create(name=name, email=self.normalize_email(email), password=password,)
-        return user
